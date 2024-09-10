42de80633125be84f54d30d27a9b0ef1394e771c
Cindy Zhang <cindyzhang.yixin@gmail.com>
2024-09-10 10:50:51 -0700

    test files

---
+++ test2.R
@@ -0,0 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: R:percent
+#     text_representation:
+#       extension: .R
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: R
+#     language: R
+#     name: ir
+# ---
+
+# %%
+library(tidyverse)
+
+# %%
